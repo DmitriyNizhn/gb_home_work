eece5f550fa27028b08a642a70bc8cc2758fe86b
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2021-12-18 11:45:27 +0300

    create

---
+++ hw_les_1.1.py
@@ -0,0 +1,44 @@
+duration = int(input())
+if duration < 60:
+    print(duration, 'сек')
+elif duration < 3600:
+    minutes = duration // 60
+    sec = duration % 60
+    print(minutes, 'мин', sec, 'сек')
+elif duration < 3600 * 24:
+    sec = duration % 60
+    minutes = duration // 60
+    hour = minutes // 60
+    minutes = minutes % 60
+    print(hour, 'час', minutes, 'мин', sec, 'сек')
+else:
+    sec = duration % 60
+    minutes = duration // 60
+    hour = minutes // 60
+    days = hour // 24
+    hour = hour % 24
+    minutes = minutes % 60
+    print(days, 'дн', hour, 'час', minutes, 'мин', sec, 'сек')
+
+# list_sec = list(map(int, input().split()))
+# for duration in list_sec:
+#     if duration < 60:
+#         print(duration, 'сек')
+#     elif duration < 3600:
+#         minutes = duration // 60
+#         sec = duration % 60
+#         print(minutes, 'мин', sec, 'сек')
+#     elif duration < 3600 * 24:
+#         sec = duration % 60
+#         minutes = duration // 60
+#         hour = minutes // 60
+#         minutes = minutes % 60
+#         print(hour, 'час', minutes, 'мин', sec, 'сек')
+#     else:
+#         sec = duration % 60
+#         minutes = duration // 60
+#         hour = minutes // 60
+#         days = hour // 24
+#         hour = hour % 24
+#         minutes = minutes % 60
+#         print(days, 'дн', hour, 'час', minutes, 'мин', sec, 'сек')
