30890f9b2eb9da037b346cb4d4af15c8d7f25a74
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2021-12-18 14:33:57 +0300

    add hw_les1.3 fixed 4 line

--- hw_les1.3.py
+++ hw_les1.3.py
@@ -1,7 +1,7 @@
 for i in range(1, 101):
     if i != 11 and i % 10 == 1:
         print(i, 'процент')
-    elif (i % 10 > 4 and i % 10 < 10) or i % 10 == 0:
+    elif (i % 10 > 4 and i % 10 < 10) or i % 10 == 0 or i // 10 == 1:
         print(i, 'процентов')
     else:
         print(i, 'процента')
