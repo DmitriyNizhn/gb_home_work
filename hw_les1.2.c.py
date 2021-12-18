5b81c439c9ef37dc0f3e2a8ab35c6e8a47687a0b
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2021-12-18 12:29:31 +0300

    add hw_les1.2.c 17 were added to each number of the list without creating a new list.

---
+++ hw_les1.2.c.py
@@ -0,0 +1,20 @@
+coub_list = []  # список нечетных чисел, в кубе
+for i in range(1, 1000):
+    if i % 2 != 0:
+        i = i ** 3
+        coub_list.append(i)
+sum_my_num = 0  # Сумма необходимых чисел
+for i in coub_list:
+    i = i + 17  # к каждому числу списка добавили 17
+    length = len(str(i))
+    sum_i_num = 0  # Сумма чисел состовляющих i число
+    copy_i = i
+    while True:
+        sum_i_num += copy_i % 10
+        copy_i //= 10
+        length -= 1
+        if length == 0:
+            break
+    if sum_i_num % 7 == 0:
+        sum_my_num += i
+print(sum_my_num)
