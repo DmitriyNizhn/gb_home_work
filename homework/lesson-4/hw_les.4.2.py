1d8a09766d870af46a7a8a8f10e0e1b7556178b5
DmitriyNizhn <94756210+DmitriyNizhn@users.noreply.github.com>
2021-12-28 22:39:41 +0300

    all done. task 2 - 2,3 hours

---
+++ homework/lesson-4/hw_les.4.2.py
@@ -0,0 +1,23 @@
+from requests import get
+from re import findall
+
+
+def currency_rates(valute):
+    valute = valute.upper()
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    content = response.content.decode(encoding=response.encoding)
+    date = str(findall(r'\d{2}.\d{2}.\d{4}', content)[0])
+    for el in content.split(f'{valute}</CharCode><Nominal>')[0:]:
+        text = el.split('</Value>')[0]
+        nominal = text.split('<')[0]
+        value = text.split('>')[-1]
+    if nominal == '':
+        return 'None'
+    else:
+        nominal = float(nominal)
+        value = float(value.replace(',', '.'))
+        value = value / nominal
+        return value, date
+
+
+print(currency_rates('usd'))
